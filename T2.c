d0db919dd9a62343ef85f59c98ea4a780124c13f
12105778 <12105778@l3043486.portoalegre.pucrsnet.br>
2017-11-08 18:58:02 -0200

    Troca para array; etc etc

--- T2.c
+++ T2.c
@@ -4,8 +4,14 @@ Engenharia Física - UFRGS  -  00218975
 Engenharia da Computação - PUCRS  - 121057780
 
 Gustavo Zanotto
-Engenhaira da Computação - PUCRS -
+Engenharia da Computação - PUCRS -
 */
+
+
+
+
+// 08/11: criar uma lista encadeada ou array global que lista os 10 PCBs
+// quando thread começar, pedir pro SO algum dos PCBs dessa lista
 
 
 // implementação da memoria em lista duplamente encadeada, mas n circular!
@@ -24,28 +30,38 @@ Engenhaira da Computação - PUCRS -
 #define N_process 10
 #define N_pages 10
 
+int total_page_fault = 0;
+int total_hit = 0;
+
+int index_ultimo_frame_escrito = 0; // variavel global para utilizar algoritmo de vitimacao FIFO
+
 typedef struct frame
 {
     int index;
+    int process_id;
     int used = 0;
     int second_chance = 0; // uso apenas para algoritmo de vitimação second-chance
 }frame;
 
-//struct frame memory[N_frames];
+
+struct frame memory[N_frames]; // array de frames
+
+
 //int iterador = 0;  // para percorrer a lista de frames
+
 sem_t mutex;  // mutex para gerenciar acesso à lista de frames
 
 
-typedef struct node_mem
+/*typedef struct node_mem
 {
 	struct node_mem * previous;
 	struct node_mem * next;
-  struct frame      frame;
+    struct frame      frame;
 }node_mem;
 
 node_mem * head_mem; // sempre vai apontar pro primeiro frame da lista
 node_mem * tail_mem; // sempre vai apontar pro ultimo frame da lista
-
+*/
 typedef struct page
 {
     int frame_index; // em qual frame a pagina esta localizada, vai de 0 a 99
@@ -54,76 +70,101 @@ typedef struct page
 
 typedef struct process_control_block // estrutura PCB, uma para cada thread
 {
-    int pid; // identificador do processo, vai ir de 0 a N_process-1
     struct page page_table[N_pages];     // o indice do page table é a página a ser lida; o conteúdo é o frame no qual a pagina se encontra
 }process_control_block;
 
+struct process_control_block PCBs[N_process];
 
+int processos_criados = 0;
 
-int EscrevePagina(process_control_block * pcb, int num_pagina) // retorna em qual frame a pagina foi escrita
+void Vitimar(int pid, int page)
 {
-  sem_wait(&mutex);
-  // como é lista encadeada, deve ser percorrida do inicio, primeiro frame vago é preenchido
-  node_mem * iterador = head_mem;
-  while((iterador->frame.used == 1)  && (iterador->frame.index <= 99)) // percorre toda lista, vai parar no primeiro com used = 0
-  {
-    iterador = iterador->next; // avança iterador para próximo!
-  }
-  // se chegou no fim significa que nao achou lugar disponivel
-  if((iterador->frame.index == 99) && (iterador->frame.used == 1))
-  {
-    // algoritmo de vitimacao
-    // ao remover determinada pagina de um frame ocupado, devemos pensar numa maneira de atualizar a page_table do processo que utilizava aquela frame
-    // para indicar que sua pagina n esta mais carregada
-  }
-  else // significa que parou antes! entao pode escrever direto
-  {
-      iterador->frame.used = 1;  // "ocupa" o frame
-      pcb->page_table[num_pagina].frame_index = iterador->frame.index;  // tabela recebe indice de qual frame a pagina esta ocupando
-      pcb->page_table[num_pagina].valid_bit = 1;  // indica que pagina esta carregada em um frame
-  }
-  sem_post(&mutex);
-}
+    if (algoritmo == 1)
+    {
+        //FIFO
+    }
 
+    else if (algoritmo == 2)
+    {
+        //LRU
+    }
 
-int LerPagina(process_control_block * pcb, int num_pagina) // retorna index do frame no qual pagina se encontra
-{
-  if(pcb->page_table[num_pagina].valid_bit == 1)  // significa que a pagina esta "carregada" em algum frame
-    {return pcb->page_table[num_pagina].frame_index} // "ler" a pagina == do nothing
     else
     {
-      EscrevePagina(pcb, num_pagina);
+        //Second chance
     }
+
+}
+
+int CarregaPagina(int pid, int page) // retorna em qual frame a pagina foi escrita
+{
+  int i = 0;
+
+    for(i=0;i<N_frames;i++)
+    {
+        if(memory[i].used == 0)
+        {
+            memory[i].used = 1;
+            PCBs[pid].page_table[page].valid_bit = 1;
+            PCBs[pid].page_table[page].frame_index = i;
+            i = 200;
+        }
+    }
+
+    if(i == N_frames) // testou todos frames, todo array memory
+    {Vitimar(pid, page);}
+}
+    /*while(memory[i].used == 1 && i<=99)
+    {
+        // frame ja ocupado, avança pro próximo
+        i++;
+    }
+
+    if(i > 99)
+    {
+        //vitimacao
+    }
+
+    else*/
+
+}
+   
+
+
+int LerPagina(int process_id, int page) // retorna index do frame no qual pagina se encontra
+{
+  if(PCBs[process_id].page_table[page].valid_bit == 1)  // significa que a pagina esta "carregada" em algum frame
+    {
+      total_hit++;
+      printf("Page %d from process %d found!\n", page, process_id);
+
+    } // "ler" a pagina == do nothing
+    else
+    {
+      total_page_fault++;
+      CarregaPagina(process_id, page);
+    }
+}
+
+int criaProcesso()
+{
+    processos_criados++;
+    return (processos_criados-1);
+
 }
 
 void * rodaProcesso(void* cont)
 {
-    int id = (int)cont;
+    int pid = criaProcesso(); // para acessar o PCB especifico, basta usar PCBs[id]
     int num_pagina;
-    process_control_block * process = malloc(sizeof(process_control_block));  // aloca nova estrutura PCB para cada thread
-    process->pid = id;
     while(1)
     {
-      num_pagina = rand()%10;  // pagina a ser lida/escrita eh aleatoria
-      if ((rand()%10)>4)
-        {EscrevePagina(process, num_pagina);}
-      else
-        {LerPagina(process, num_pagina);}
+      num_pagina = rand()%N_pages;  // pagina a ser lida eh aleatoria
+     
+      sem_wait(&mutex);
+      LerPagina(pid, num_pagina);
+      sem_post(&mutex);
     }
-}
-
-
-void initMemory() // adiciona 98 nodos
-{
-  for(int i=1;i<N_frames-1;i++)
-  {
-    node_mem * new = malloc(sizeof(node_mem)); // aloca novo nodo e posiciona antes do ultimo
-    new->next = tail_mem;
-    new->previous = tail_mem->previous;
-    tail_mem->previous->next = new;
-    tail_mem->previous = new;
-    new->frame.index = i;
-  }
 }
 
 
@@ -139,19 +180,6 @@ int main()
     pthread_t tid[N_process];
     pthread_attr_init(&attr);
 
-    head_mem = malloc(sizeof(node_mem));
-    tail_mem = malloc(sizeof(node_mem));
-
-    head_mem->frame.index = 0;
-    tail_mem->frame.index = 99;
-    head_mem->next = tail_mem;
-    tail_mem->previous = head_mem;
-    initMemory();
-
-    /*for(int i = 0; i<N_frames;i++)
-    {
-        node_mem *
-    }*/
     while(cont < N_process) // loop para criar todas as threads
     {
         pthread_create(&(tid[cont]), &attr, rodaProcesso, cont);
